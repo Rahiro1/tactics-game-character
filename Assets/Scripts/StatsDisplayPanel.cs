3529e1bcc40e500a4a3649f00568d527d4b984dd
James <jdt130@bham.ac.uk>
2025-03-30 00:06:45 +0000

    Add onStart to stats display and fix indexing bug

--- Assets/Scripts/StatsDisplayPanel.cs
+++ Assets/Scripts/StatsDisplayPanel.cs
@@ -6,26 +6,36 @@ public class StatsDisplayPanel : MonoBehaviour
 {
     [SerializeField]
     public List<StatDisplay> statDisplays;
+    public CharacterSO displayCharacter;
+
+
+    private void Start()
+    {
+        if (displayCharacter != null)
+        {
+            DisplayAllStats(new Character(displayCharacter));
+        }
+    }
 
     public void DisplayAllStats(Character character)
     {
         statDisplays[0].DisplayStat("MaxHP", character.MaxHP.GetModifiedValue());
-        statDisplays[0].DisplayStat("Strength", character.Strength.GetModifiedValue());
-        statDisplays[0].DisplayStat("Magic", character.Magic.GetModifiedValue());
-        statDisplays[0].DisplayStat("OFF", character.Offence.GetModifiedValue());
-        statDisplays[0].DisplayStat("DEF", character.Defence.GetModifiedValue());
-        statDisplays[0].DisplayStat("RES", character.Resistance.GetModifiedValue());
-        statDisplays[0].DisplayStat("Speed", character.Speed.GetModifiedValue());
-        statDisplays[0].DisplayStat("Move", character.Move.GetModifiedValue());
-        statDisplays[0].DisplayStat("Armour", character.MaxArmour);
-        statDisplays[0].DisplayStat("Attack", character.Attack);
-        statDisplays[0].DisplayStat("OFH", character.OffensiveHit);
-        statDisplays[0].DisplayStat("DFH", character.DefensiveHit);
-        statDisplays[0].DisplayStat("Crit", character.CriticalRate);
-        statDisplays[0].DisplayStat("CritAvoid", character.CriticalAvoid);
-        statDisplays[0].DisplayStat("Guard", character.Guard);
-        statDisplays[0].DisplayStat("Wield", character.Wield);
-        statDisplays[0].DisplayStat("Rending", character.Rending);
-        statDisplays[0].DisplayStat("Range", character.Range);
+        statDisplays[1].DisplayStat("Strength", character.Strength.GetModifiedValue());
+        statDisplays[2].DisplayStat("Magic", character.Magic.GetModifiedValue());
+        statDisplays[3].DisplayStat("OFF", character.Offence.GetModifiedValue());
+        statDisplays[4].DisplayStat("DEF", character.Defence.GetModifiedValue());
+        statDisplays[5].DisplayStat("RES", character.Resistance.GetModifiedValue());
+        statDisplays[6].DisplayStat("Speed", character.Speed.GetModifiedValue());
+        statDisplays[7].DisplayStat("Move", character.Move.GetModifiedValue());
+        statDisplays[8].DisplayStat("Armour", character.MaxArmour);
+        statDisplays[9].DisplayStat("Attack", character.Attack);
+        statDisplays[10].DisplayStat("OFH", character.OffensiveHit);
+        statDisplays[11].DisplayStat("DFH", character.DefensiveHit);
+        statDisplays[12].DisplayStat("Crit", character.CriticalRate);
+        statDisplays[13].DisplayStat("CritAvoid", character.CriticalAvoid);
+        statDisplays[14].DisplayStat("Guard", character.Guard);
+        statDisplays[15].DisplayStat("Wield", character.Wield);
+        statDisplays[16].DisplayStat("Rending", character.Rending);
+        statDisplays[17].DisplayStat("Range", character.Range);
     }
 }
